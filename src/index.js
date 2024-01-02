7ad4b8b6d4c314d7b0c4f808af222a2423dd6146
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-02 23:20:58 +0300

    Keeper App Project - Part 1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+//which is the same as below
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+  <App />
+  </div>
+  );
