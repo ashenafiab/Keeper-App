cc4d01f102342a0c5603d243d375c0f0a32ad98b
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-26 14:41:25 +0300

    Keeper App Project - Part 3

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,45 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-// console.log(notes);
+import CreateArea from "./CreateArea";
 
 function App() {
-    return <div>
-        <Header />
-        {notes.map(noteItem =>
-            <Note
-                key={noteItem.key}
-                title={noteItem.title}
-                content={noteItem.content}
+
+    const [notes, setNotes] = React.useState([]);
+
+    function addNote(note) {
+        setNotes(prevNotes => {
+            return [...prevNotes, note];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea
+                onAdd={addNote}
             />
-        )
-        }
-        <Footer />
-    </div>
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content} onDelete={deleteNote}
+                    />
+                );
+            })}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
